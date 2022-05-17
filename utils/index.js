7f69dcede0b27b379a3ebddde10060578b267595
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-05-16 21:44:48 -0300

    adicionado 'writeData' ao indice dos utilitarios

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 const readData = require('./readData');
 const generateToken = require('./generateToken');
+const writeData = require('./writeData');
 
-module.exports = { readData, generateToken };
+module.exports = { readData, generateToken, writeData };
