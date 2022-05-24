4308243e9534ae529200091d971b6a44cc0e8f79
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-05-24 20:46:50 -0300

    removido todos os NEXT das rotas

--- routes/talker.js
+++ routes/talker.js
@@ -4,20 +4,18 @@ const { authToken, validationTalker } = require('../middlewares');
 const routes = express.Router();
 const { readData, writeData } = require('../utils');
 
-routes.get('/search', authToken, (req, res, next) => {
+routes.get('/search', authToken, (req, res) => {
   const { q } = req.query;
   console.log(q);
   const filtered = readData().filter(({ name }) => name.includes(q));
   res.status(200).json(filtered);
-  next();
 });
 
-routes.get('/', (_req, res, next) => {
+routes.get('/', (_req, res) => {
   res.status(200).json(readData());
-  next();
 });
 
-routes.get('/:id', (req, res, next) => {
+routes.get('/:id', (req, res) => {
   const { id } = req.params;
 
   const filteredId = readData().filter((data) => data.id === +id)[0];
@@ -26,10 +24,9 @@ routes.get('/:id', (req, res, next) => {
   }
 
   res.status(200).json(filteredId);
-  next();
 });
 
-routes.post('/', authToken, validationTalker, (req, res, next) => {
+routes.post('/', authToken, validationTalker, (req, res) => {
   const { body: newTalker } = req;
   const data = readData();
 
@@ -39,10 +36,9 @@ routes.post('/', authToken, validationTalker, (req, res, next) => {
   writeData('./talker.json', data);
 
   res.status(201).json(newTalker);
-  next();
 });
 
-routes.put('/:id', authToken, validationTalker, (req, res, next) => {
+routes.put('/:id', authToken, validationTalker, (req, res) => {
   const { body: newTalker } = req;
   const { id } = req.params;
   const data = readData();
@@ -55,7 +51,6 @@ routes.put('/:id', authToken, validationTalker, (req, res, next) => {
   writeData('./talker.json', newData);
 
   res.status(200).json(newTalker);
-  next();
 });
 
 routes.delete('/:id', authToken, async (req, res) => {
